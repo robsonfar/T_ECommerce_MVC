7732ac73e02e54bc115b674b00f41455329954aa
Robson Farias <robsonfar@hotmail.com>
2024-10-10 06:42:27 -0300

    Add project files.

---
+++ T_ECommerce_MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace T_ECommerce_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
