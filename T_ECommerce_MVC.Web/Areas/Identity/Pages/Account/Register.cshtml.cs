a81f5fb2e688770674eec784649107a1133a7b83
Farias, Robson <robsonfar@hotmail.com>
2024-10-17 16:15:44 -0300

    Update Login and Register.

--- T_ECommerce_MVC.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ T_ECommerce_MVC.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -101,9 +101,16 @@ namespace T_ECommerce_MVC.Web.Areas.Identity.Pages.Account
             public string ConfirmPassword { get; set; }
 
             public string? Role { get; set; }
-
             [ValidateNever]
             public IEnumerable<SelectListItem> RoleList { get; set; }
+
+            [Required]
+            public string Name { get; set; }
+            public string? StreetAddress { get; set; }
+            public string? City { get; set; }
+            public string? State { get; set; }
+            public string? PostalCode { get; set; }
+            public string? PhoneNumber { get; set; }
 
         }
 
@@ -141,6 +148,14 @@ namespace T_ECommerce_MVC.Web.Areas.Identity.Pages.Account
 
                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
+
+                user.StreetAddress = Input.StreetAddress;
+                user.City = Input.City;
+                user.Name = Input.Name;
+                user.State = Input.State;
+                user.PostalCode = Input.PostalCode;
+                user.PhoneNumber = Input.PhoneNumber;
+
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
                 if (result.Succeeded)
